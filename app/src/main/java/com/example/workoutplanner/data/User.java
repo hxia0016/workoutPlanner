b2e631a60cd25f105fa53c1545d0dd1112e42276
hxia0016 <hxia0016@student.monash.edu>
2022-05-02 01:02:58 +0800

    firebase set up and register user

---
+++ app/src/main/java/com/example/workoutplanner/data/User.java
@@ -0,0 +1,50 @@
+package com.example.workoutplanner.data;
+
+public class User {
+
+    public String fName, lName, age, email;
+
+    public User(){
+
+    }
+
+    public User(String fName, String lName, String age, String email) {
+        this.fName = fName;
+        this.lName = lName;
+        this.age = age;
+        this.email = email;
+    }
+
+    public String getfName() {
+        return fName;
+    }
+
+    public void setfName(String fName) {
+        this.fName = fName;
+    }
+
+    public String getlName() {
+        return lName;
+    }
+
+    public void setlName(String lName) {
+        this.lName = lName;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
+
