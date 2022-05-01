0ad9631d14a0bb98b1bf5102b51431e4357dba69
hxia0016 <hxia0016@student.monash.edu>
2022-05-02 03:30:48 +0800

    added forget password feature

--- app/src/main/java/com/example/workoutplanner/MainActivity.java
+++ app/src/main/java/com/example/workoutplanner/MainActivity.java
@@ -1,15 +1,34 @@
 package com.example.workoutplanner;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.workoutplanner.UserSignIn.ForgetPassword;
+import com.example.workoutplanner.UserSignIn.RegisterUser;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
-    private TextView register;
+    private TextView register, forget;
+    private EditText userEmail, userPassword;
+    private Button login;
+
+    private FirebaseAuth mAuth;
+    private ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +37,19 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         register = (TextView) findViewById(R.id.register);
         register.setOnClickListener(this);
+
+        forget = (TextView) findViewById(R.id.forgetPassword);
+        forget.setOnClickListener(this);
+
+        login = (Button) findViewById(R.id.login);
+        login.setOnClickListener(this);
+
+        userEmail = (EditText) findViewById(R.id.email);
+        userPassword = (EditText) findViewById(R.id.password);
+
+        progressBar = (ProgressBar) findViewById(R.id.mainProgressBar);
+
+        mAuth = FirebaseAuth.getInstance();
     }
 
 
@@ -26,6 +58,76 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         switch (view.getId()){
             case R.id.register:
                 startActivity(new Intent(this, RegisterUser.class));
+                break;
+                
+            case R.id.login:
+                userLogin();
+                break;
+
+            case R.id.forgetPassword:
+                startActivity(new Intent(this, ForgetPassword.class));
+                break;
         }
+    }
+
+    private void userLogin() {
+        String email = userEmail.getText().toString().trim();
+        String password = userPassword.getText().toString().trim();
+
+        if(email.isEmpty()){
+            userEmail.setError("Email is required!");
+            userEmail.requestFocus();
+            return;
+        }
+
+        if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            userEmail.setError("Please enter a valid email address!");
+            userEmail.requestFocus();
+            return;
+        }
+
+        if(password.isEmpty()){
+            userPassword.setError("Password is required!");
+            userPassword.requestFocus();
+            return;
+        }
+
+        if(password.length()<6){
+            userPassword.setError("Password enter at least 6 character for password!");
+            userPassword.requestFocus();
+            return;
+        }
+
+        progressBar.setVisibility(View.VISIBLE);
+
+        mAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    //delete last two lines and uncomment to use email verification
+                    FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                    startActivity(new Intent(MainActivity.this, HomePage.class));
+                    progressBar.setVisibility(View.GONE);
+                    /*
+                    if(user.isEmailVerified()){
+                        startActivity(new Intent(MainActivity.this, HomePage.class));
+                        progressBar.setVisibility(View.GONE);
+                    }
+                    else{
+                        user.sendEmailVerification();
+                        Toast.makeText(MainActivity.this, "Check your Email to verify!",
+                                Toast.LENGTH_LONG).show();
+                        progressBar.setVisibility(View.GONE);
+                    }
+
+                     */
+                }
+                else{
+                    Toast.makeText(MainActivity.this, "Failed to login! Please check!",
+                            Toast.LENGTH_LONG).show();
+                    progressBar.setVisibility(View.GONE);
+                }
+            }
+        });
     }
 }
