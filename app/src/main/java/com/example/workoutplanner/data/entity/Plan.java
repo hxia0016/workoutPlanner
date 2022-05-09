4e5ec94a651a9fab2562db68081a197058cdf4f9
hxia0016 <hxia0016@student.monash.edu>
2022-05-09 14:06:53 +0800

    updated register input information,added spinner, room initialized

---
+++ app/src/main/java/com/example/workoutplanner/data/entity/Plan.java
@@ -0,0 +1,54 @@
+package com.example.workoutplanner.data.entity;
+
+import androidx.annotation.NonNull;
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity
+public class Plan {
+    @PrimaryKey(autoGenerate = true)
+    public int uid;
+    @ColumnInfo(name = "Activity")
+    @NonNull
+    public String activity;
+    @ColumnInfo(name = "Duration")
+    @NonNull
+    public int duration;
+    @ColumnInfo(name = "Complete")
+    @NonNull
+    public boolean complete;
+
+    public Plan(@NonNull String activity, @NonNull int duration, @NonNull boolean complete) {
+        this.activity = activity;
+        this.duration = duration;
+        this.complete = complete;
+    }
+
+    @NonNull
+    public String getActivity() {
+        return activity;
+    }
+
+    public void setActivity(@NonNull String activity) {
+        this.activity = activity;
+    }
+
+    @NonNull
+    public int getDuration() {
+        return duration;
+    }
+
+    public void setDuration(@NonNull int duration) {
+        this.duration = duration;
+    }
+
+    @NonNull
+    public boolean isComplete() {
+        return complete;
+    }
+
+    public void setComplete(@NonNull boolean complete) {
+        this.complete = complete;
+    }
+}
