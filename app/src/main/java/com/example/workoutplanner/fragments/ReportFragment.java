6d97ddbc386c7536e74ec1b60340a9750fe177f4
Ruifan Lu <rluu0003@student.monash.edu>
2022-05-14 23:14:52 +0800

    change UI

--- app/src/main/java/com/example/workoutplanner/fragments/ReportFragment.java
+++ app/src/main/java/com/example/workoutplanner/fragments/ReportFragment.java
@@ -14,6 +14,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.DatePicker;
+import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
